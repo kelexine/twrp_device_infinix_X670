229092c75cc85d22ee9225277105ec37f311d215
kelexine <frankiekelechi@gmail.com>
2024-03-28 09:31:37 +0100

    fix product packages

--- device.mk
+++ device.mk
@@ -16,31 +16,42 @@ AB_OTA_PARTITIONS += \
     vbmeta_vendor \
     boot \
     system
-    
+
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
-    
+
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Virtual A/B
 ENABLE_VIRTUAL_AB := true
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
-# Boot control HAL
+# Health HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service \
+    libhealthd.$(PRODUCT_PLATFORM)
 
+# Boot Control HAL
 PRODUCT_PACKAGES += \
+    android.hardware.boot@1.1-mtkimpl.recovery \
+    android.hardware.boot@1.1-mtkimpl \
+    bootctl
+
+PRODUCT_PACKAGES_DEBUG += \
     bootctrl.mt6781
 
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.mt6781 \
-    libgptutils \
-    libz \
-    libcutils
+# Fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
+
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
